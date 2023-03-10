c47bc5d60c7d9b12c277216346180429b320e3ad
akashbagchi <72804789+akashbagchi@users.noreply.github.com>
2023-03-10 12:28:12 +0530

    Trying to account for no :has support

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,27 @@ const colorThemes = document.querySelectorAll('[name="theme"]');
 // store theme
 const storeTheme = function(theme) {
     localStorage.setItem("theme", theme);
+};
+
+// fallback for no has support
+const setTheme = function(theme) {
+
 }
 
-colorThemes.forEach(themeOption => {
+const retrieveTheme = function() {
+    const activeTheme = localStorage.getItem("theme");
+    colorThemes.forEach((themeOption) => {
+        if (themeOption.id === activeTheme) {
+            themeOption.ariaChecked = true;
+        }
+    });
+};
+
+colorThemes.forEach((themeOption) => {
     themeOption.addEventListener('click', () => {
         storeTheme(themeOption.id);
-    })
-})
+    });
+});
 
 // apply theme
+document.onload = retrieveTheme();
